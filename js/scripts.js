b879967cc34afec7591e09826ae23370b2b8a6f4
Tabitha L. Leonard <tabitha.l.leonard14@gmail.com>
2022-06-01 20:44:36 -0600

    Added Hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hi! Welcome to My Portfolio Website, take a look around.")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle'){
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
