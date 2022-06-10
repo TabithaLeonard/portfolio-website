30468b56e1511de1d1f9daebdca2a7c7133404c4
Tabitha L. Leonard <tabitha.l.leonard14@gmail.com>
2022-06-09 19:27:09 -0600

    Linter Changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
-console.log("Hi! Welcome to My Portfolio Website, take a look around.")
-function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle'){
-        x.className += ' responsive';
-    } else {
-        x.className = 'navtoggle';
-    }
+console.log('Hi! Welcome to My Portfolio Website, take a look around.')
+// eslint-disable-next-line no-unused-vars
+function menuToggle () {
+  const x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
 }
